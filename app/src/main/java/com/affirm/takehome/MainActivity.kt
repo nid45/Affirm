f4dd25685a84b44751b3cf95f1f29eb2c36176bf
nid45 <nid45@pitt.edu>
2021-02-08 23:51:38 -0500

    first

--- app/src/main/java/com/affirm/takehome/MainActivity.kt
+++ app/src/main/java/com/affirm/takehome/MainActivity.kt
@@ -13,6 +13,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.size
 import androidx.lifecycle.ViewModelProvider
 import com.affirm.takehome.adapter.RestaurantAdapter
 import com.affirm.takehome.data.Restaurant
@@ -31,9 +32,7 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import retrofit2.Response
 import kotlin.properties.Delegates.observable
 
@@ -41,13 +40,13 @@ private const val LOCATION_PERMISSION_CODE = 101
 private const val THUMB_UP = R.drawable.thumb_up
 private const val THUMB_DOWN = R.drawable.thumb_down
 private const val TAG = "MainActivity"
-private var isYelp = true
 lateinit var viewModel: MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private var yelpApi: YelpRestaurantApi = YelpRestaurantApiFactory.create()
     private var zomatoApi: ZomatoRestaurantApi = ZomatoRestaurantApiFactory.create()
     lateinit var loc: Location
+    private var isYelp = false
 
 
     private var animating = false
@@ -92,60 +91,23 @@ class MainActivity : AppCompatActivity() {
                 yesCounter++
                 viewPager.currentItem = viewPager.currentItem + 1
                 animateIcon(THUMB_UP)
-            }
-            if((viewModel.restaurantList.value?.size?.minus(viewPager.currentItem))!! < 3) {
-                if (isYelp) {
-                    runBlocking{
-                        withContext(Dispatchers.Default) {
-                            var yelpResponse =
-                                yelpApi.getRestaurants(loc.latitude, loc.longitude, 0)
-                                    .execute()
-                            yelpResponse.body()?.restaurants?.let { yelpToRestaurant(it) }
-                        }
+                if((viewModel.restaurantList.value?.size?.minus(viewPager.currentItem))!! == 3) {
+                    CoroutineScope(Dispatchers.Default).launch {
+                        getData()
                     }
-                    (viewPager.adapter as RestaurantAdapter).notifyDataSetChanged()
-                } else {
-                    runBlocking{
-                        withContext(Dispatchers.Default) {
-                            var zomatoResponse =
-                                zomatoApi.getRestaurants(loc.latitude, loc.longitude, 0)
-                                    .execute()
-                            zomatoResponse.body()?.restaurants?.let { zomatoToRestaurant(it) }
-                        }
-                    }
-                    (viewPager.adapter as RestaurantAdapter).notifyDataSetChanged()
                 }
             }
         }
 
-        noButton.setOnClickListener {
+        noButton.setOnClickListener{
             if (!animating) {
                 noCounter++
                 viewPager.currentItem = viewPager.currentItem + 1
                 animateIcon(THUMB_DOWN)
-            }
-
-            if((viewModel.restaurantList.value?.size?.minus(viewPager.currentItem))!! < 3) {
-                if (isYelp) {
-                    runBlocking{
-                        withContext(Dispatchers.Default) {
-                            var yelpResponse =
-                                yelpApi.getRestaurants(loc.latitude, loc.longitude, 0)
-                                    .execute()
-                            yelpResponse.body()?.restaurants?.let { yelpToRestaurant(it) }
-                        }
+                if((viewModel.restaurantList.value?.size?.minus(viewPager.currentItem))!! == 3) {
+                    CoroutineScope(Dispatchers.Default).launch {
+                        getData()
                     }
-                    (viewPager.adapter as RestaurantAdapter).notifyDataSetChanged()
-                } else {
-                    runBlocking{
-                        withContext(Dispatchers.Default) {
-                            var zomatoResponse =
-                                zomatoApi.getRestaurants(loc.latitude, loc.longitude, 0)
-                                    .execute()
-                            zomatoResponse.body()?.restaurants?.let { zomatoToRestaurant(it) }
-                        }
-                    }
-                    (viewPager.adapter as RestaurantAdapter).notifyDataSetChanged()
                 }
             }
         }
@@ -228,7 +190,6 @@ class MainActivity : AppCompatActivity() {
                                     loc = location
                                     var yelpResponse = yelpApi.getRestaurants(location.latitude, location.longitude, 0).execute()
                                     yelpResponse.body()?.restaurants?.let { yelpToRestaurant(it) }
-                                    restaurantAdapter.addRestaurants(viewModel.restaurantList.value!!)
                                     viewPager.adapter = restaurantAdapter
 
                                     true
@@ -242,49 +203,115 @@ class MainActivity : AppCompatActivity() {
             } else {
                 loc = location
                 runBlocking{
+                    runOnUiThread {
+                        viewPager.adapter = restaurantAdapter
+                    }
                     withContext(Dispatchers.Default) {
                             var yelpResponse =
                                 yelpApi.getRestaurants(location.latitude, location.longitude, 0)
                                     .execute()
                             yelpResponse.body()?.restaurants?.let { yelpToRestaurant(it) }
 
-
                     }
                     runOnUiThread {
-                        restaurantAdapter.itemCount
-                        restaurantAdapter.addRestaurants(viewModel.restaurantList.value!!)
-                        viewPager.adapter = restaurantAdapter
+                        viewPager.adapter?.notifyDataSetChanged()
                     }
+
                 }
             }
         }
     }
 
+    /**
+     * Takes a list of YelpRetsaurant objects as returned from the Yelp API
+     * and places them into the viewModel as Restaurant objects
+     * so that the adapter will recognize them as new elements
+     *
+     * @param  list the list of YelpRestaurants
+     * @return no return value
+     */
     fun yelpToRestaurant(list: List<YelpRestaurant>) {
+        var temp: MutableList<Restaurant> = mutableListOf()
+
         for(restaurant in list) {
-            viewModel.restaurantList.value?.add(
-                Restaurant(
+            var rest = Restaurant(
                     restaurant.id,
                     restaurant.name,
                     restaurant.image,
                     restaurant.rating
-                )
             )
+
+            viewModel.restaurantList.value?.add(rest)
+            temp.add(rest)
+        }
+        runOnUiThread {
+            restaurantAdapter.addRestaurants(temp)
         }
     }
 
+    /**
+     * Takes a list of ZomatoRetsaurant objects as returned from the Zomato API
+     * and places them into the viewModel as Restaurant objects
+     * so that the adapter will recognize them as new elements
+     *
+     * @param  list the list of ZomatoRestaurants
+     * @return no return value
+     */
     fun zomatoToRestaurant(list: List<ZomatoRestaurant>) {
+        var temp: MutableList<Restaurant> = mutableListOf()
+
         for(restaurant in list) {
-            viewModel.restaurantList.value?.add(
-                Restaurant(
+                var rest = Restaurant(
                     restaurant.restaurantDetail.id,
                     restaurant.restaurantDetail.name,
                     restaurant.restaurantDetail.image,
                     restaurant.restaurantDetail.userRating.toString()
-                )
             )
+            viewModel.restaurantList.value?.add(rest)
+            temp.add(rest)
+        }
+        runOnUiThread {
+            restaurantAdapter.addRestaurants(temp)
         }
     }
 
 
+    /**
+     * when there are less than 3 remaining restaurant left to be shown this method will
+     * be called. It will first check the isYelp boolean variable to see if data needs to be
+     * pulled from Yelp, while if that variable is false data will be pulled from Zomato
+     * @param no params
+     * @return no return value
+     */
+    fun getData(){
+        Log.i("fuck", isYelp.toString())
+        if (isYelp) {//check if we are pulling from Yelp
+            runBlocking{
+                withContext(Dispatchers.Default) {
+                    var yelpResponse =
+                        yelpApi.getRestaurants(loc.latitude, loc.longitude, 0)
+                            .execute()
+                    yelpResponse.body()?.restaurants?.let { yelpToRestaurant(it) }
+                }
+            }
+            //this data was pulled from Yelp so we must change isYelp to false so the next data comes from Zomato
+            runOnUiThread {
+                (viewPager.adapter as RestaurantAdapter).notifyDataSetChanged()
+            }
+        } else {
+            runBlocking{
+                withContext(Dispatchers.Default) {
+                    var zomatoResponse =
+                        zomatoApi.getRestaurants(loc.latitude, loc.longitude, 0)
+                            .execute()
+                    zomatoResponse.body()?.restaurants?.let { zomatoToRestaurant(it) }
+                }
+            }
+            //this data was pulled from Zomato so we must change isYelp to true so the next data comes from Yelp
+            runOnUiThread {
+                (viewPager.adapter as RestaurantAdapter).notifyDataSetChanged()
+            }
+        }
+        isYelp = !isYelp
+    }
 }
